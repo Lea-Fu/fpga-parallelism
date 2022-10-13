e1a738da6d2bb25ba24bf231e755feae218a6bec
Lea Fuhrmann <leafuhrmann@MBP-von-Jonas.fritz.box>
2022-10-13 12:20:38 +0200

    added working CMakeList for OpenCL

---
+++ parallel_sort/OpenCL/src/main.cpp
@@ -0,0 +1,20 @@
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+/*#if defined(__APPLE__) || defined(__MACOSX)
+#include </Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk/System/Library/Frameworks/OpenCL.framework/Headers/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+ */
+
+
+int main(void) {
+
+}
+
+
+
